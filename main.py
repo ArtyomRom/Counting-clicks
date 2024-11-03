46e4329e7e88af04481b94d23b7d9acb1c36555e
Artyom <zik21b@gmail.com>
2024-11-03 17:57:25 +0300

    Counting-clicks version 1.0

---
+++ main.py
@@ -0,0 +1,43 @@
+import re
+
+from dotenv import load_dotenv
+import requests
+import os
+
+
+def is_shorten_link(token, url):
+    params = {'access_token': token, 'url': url, 'v': '5.199'}
+    response = requests.get('https://api.vk.com/method/utils.checkLink', params=params)
+    response.raise_for_status()
+    return response.json()['response']['link']
+
+def shorten_link(token, url):
+    params = {'access_token': token, 'url': url, 'private': '0', 'v': '5.199'}
+    checking_response = requests.get(url)
+    checking_response.raise_for_status()
+    response = requests.get('https://api.vk.com/method/utils.getShortLink', params=params)
+    response.raise_for_status()
+    return response.json()['response']['short_url']
+
+
+def count_clicks(token, link):
+    link = re.sub('http[s]?://vk.cc/', '', link)
+    params = {'access_token': token, 'key': link, 'v': '5.199'}
+    response = requests.get('https://api.vk.com/method/utils.getLinkStats', params=params)
+    response.raise_for_status()
+    clicks_count = response.json()['response']['stats'][0]['views']
+    return clicks_count
+
+def main():
+    load_dotenv()
+    try:
+        link = is_shorten_link(os.getenv('API_VK'), input())
+        short_link = shorten_link(os.getenv('API_VK'), link)
+        print('Сокращенная ссылка: ', short_link)
+        count_click = count_clicks(os.getenv('API_VK'), short_link)
+        print('Количество кликов: ', count_click)
+    except requests.exceptions.HTTPError as error:
+        print(error)
+
+if __name__ == '__main__':
+    main()
